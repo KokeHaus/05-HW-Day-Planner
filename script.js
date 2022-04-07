9df2c998e9bb10895d7f17cca58b12e666ffb644
KokeHaus <austinbcw@gmail.com>
2022-04-07 14:37:30 -0500

    ugly code

---
+++ script.js
@@ -0,0 +1,422 @@
+// Initial Commit
+
+
+var task0=$('#t0');
+var task1=$('#t1');
+var task2=$('#t2');
+var task3=$('#t3');
+var task4=$('#t4');
+var task5=$('#t5');
+var task6=$('#t6');
+var task7=$('#t7');
+var task8=$('#t8');
+var task9=$('#t9');
+var task10=$('#t10');
+var task11=$('#t11');
+var task12=$('#t12');
+var task13=$('#t13');
+var task14=$('#t14');
+var task15=$('#t15');
+var task16=$('#t16');
+var task17=$('#t17');
+var task18=$('#t18');
+var task19=$('#t19');
+var task20=$('#t20');
+var task21=$('#t21');
+var task22=$('#t22');
+var task23=$('#t23');
+
+var time = moment().format("HH")
+console.log(time);
+
+var task0content=$('#task0')
+var savebutton0=$('#savebutton0')
+var content0 =localStorage.getItem('task0')
+$('#task0').val(content0);
+task0.textContent=content0;
+$('#savebutton0').click(function(){
+  localStorage.setItem('task0', task0content.val())
+})
+var task1content=$('#task1')
+var savebutton1=$('#savebutton1')
+var content1 =localStorage.getItem('task1')
+$('#task1').val(content1);
+task1.textContent=content1;
+$('#savebutton1').click(function(){
+  localStorage.setItem('task1', task1content.val())
+})
+var task2content=$('#task2')
+var savebutton2=$('#savebutton2')
+var content2 =localStorage.getItem('task2')
+$('#task2').val(content2);
+task2.textContent=content2;
+$('#savebutton2').click(function(){
+  localStorage.setItem('task2', task2content.val())
+})
+var task3content=$('#task3')
+var savebutton3=$('#savebutton3')
+var content3 =localStorage.getItem('task3')
+$('#task3').val(content3);
+task3.textContent=content3;
+$('#savebutton3').click(function(){
+  localStorage.setItem('task3', task3content.val())
+})
+var task4content=$('#task4')
+var savebutton4=$('#savebutton4')
+var content4 =localStorage.getItem('task4')
+$('#task4').val(content4);
+task4.textContent=content4;
+$('#savebutton4').click(function(){
+  localStorage.setItem('task4', task4content.val())
+})
+var task5content=$('#task5')
+var savebutton5=$('#savebutton5')
+var content5 =localStorage.getItem('task5')
+$('#task5').val(content5);
+task5.textContent=content5;
+$('#savebutton5').click(function(){
+  localStorage.setItem('task5', task5content.val())
+})
+var task6content=$('#task6')
+var savebutton6=$('#savebutton6')
+var content6 =localStorage.getItem('task6')
+$('#task6').val(content6);
+task6.textContent=content6;
+$('#savebutton6').click(function(){
+  localStorage.setItem('task6', task6content.val())
+})
+var task7content=$('#task7')
+var savebutton7=$('#savebutton7')
+var content7 =localStorage.getItem('task7')
+$('#task7').val(content7);
+task7.textContent=content7;
+$('#savebutton7').click(function(){
+  localStorage.setItem('task7', task7content.val())
+})
+var task8content=$('#task8')
+var savebutton8=$('#savebutton8')
+var content8 =localStorage.getItem('task8')
+$('#task8').val(content8);
+task8.textContent=content8;
+$('#savebutton8').click(function(){
+  localStorage.setItem('task8', task8content.val())
+})
+var task9content=$('#task9')
+var savebutton9=$('#savebutton9')
+var content9 =localStorage.getItem('task9')
+$('#task9').val(content9);
+task9.textContent=content9;
+$('#savebutton9').click(function(){
+  localStorage.setItem('task9', task9content.val())
+})
+var task10content=$('#task10')
+var savebutton10=$('#savebutton10')
+var content10 =localStorage.getItem('task10')
+$('#task10').val(content10);
+task10.textContent=content10;
+$('#savebutton10').click(function(){
+  localStorage.setItem('task10', task10content.val())
+})
+var task11content=$('#task11')
+var savebutton11=$('#savebutton11')
+var content11 =localStorage.getItem('task11')
+$('#task11').val(content11);
+task11.textContent=content11;
+$('#savebutton11').click(function(){
+  localStorage.setItem('task11', task11content.val())
+})
+var task12content=$('#task12')
+var savebutton12=$('#savebutton12')
+var content12 =localStorage.getItem('task12')
+$('#task12').val(content12);
+task12.textContent=content12;
+$('#savebutton12').click(function(){
+  localStorage.setItem('task12', task12content.val())
+})
+var task13content=$('#task13')
+var savebutton13=$('#savebutton13')
+var content13 =localStorage.getItem('task13')
+$('#task13').val(content13);
+task13.textContent=content13;
+$('#savebutton13').click(function(){
+  localStorage.setItem('task13', task13content.val())
+})
+var task14content=$('#task14')
+var savebutton14=$('#savebutton14')
+var content14 =localStorage.getItem('task14')
+$('#task14').val(content14);
+task14.textContent=content14;
+$('#savebutton14').click(function(){
+  localStorage.setItem('task14', task14content.val())
+})
+var task15content=$('#task15')
+var savebutton15=$('#savebutton15')
+var content15 =localStorage.getItem('task15')
+$('#task15').val(content15);
+task15.textContent=content15;
+$('#savebutton15').click(function(){
+  localStorage.setItem('task15', task15content.val())
+})
+var task16content=$('#task16')
+var savebutton16=$('#savebutton16')
+var content16 =localStorage.getItem('task16')
+$('#task16').val(content16);
+task16.textContent=content16;
+$('#savebutton16').click(function(){
+  localStorage.setItem('task16', task16content.val())
+})
+var task17content=$('#task17')
+var savebutton17=$('#savebutton17')
+var content17 =localStorage.getItem('task17')
+$('#task17').val(content17);
+task17.textContent=content17;
+$('#savebutton17').click(function(){
+  localStorage.setItem('task17', task17content.val())
+})
+var task18content=$('#task18')
+var savebutton18=$('#savebutton18')
+var content18 =localStorage.getItem('task18')
+$('#task18').val(content18);
+task18.textContent=content18;
+$('#savebutton18').click(function(){
+  localStorage.setItem('task18', task18content.val())
+})
+var task19content=$('#task19')
+var savebutton19=$('#savebutton19')
+var content19 =localStorage.getItem('task19')
+$('#task19').val(content19);
+task19.textContent=content19;
+$('#savebutton19').click(function(){
+  localStorage.setItem('task19', task19content.val())
+})
+var task20content=$('#task20');
+var savebutton20=$('#savebutton20')
+var content20=localStorage.getItem('task20')
+$('#task20').val(content20);
+task0.textContent=content20;
+$('#savebutton20').click(function(){
+  localStorage.setItem('task20', task20content.val())
+})
+var task21content=$('#task21');
+var savebutton21=$('#savebutton21')
+var content21=localStorage.getItem('task21')
+$('#task21').val(content21);
+task21.textContent=content21;
+$('#savebutton21').click(function(){
+  localStorage.setItem('task21', task21content.val())
+})
+var task22content=$('#task22');
+var savebutton22=$('#savebutton22')
+var content22=localStorage.getItem('task22')
+$('#task22').val(content22);
+task2.textContent=content22;
+$('#savebutton22').click(function(){
+  localStorage.setItem('task22', task20content.val())
+})
+var task23content=$('#task23');
+var savebutton23=$('#savebutton23')
+var content23=localStorage.getItem('task23')
+$('#task23').val(content23);
+task23.textContent=content23;
+$('#savebutton23').click(function(){
+  localStorage.setItem('task23', task20content.val())
+})
+
+
+console.log($('#time2').html())
+
+if($('#time00').html()<time){
+task0.addClass("table-danger")
+task0.removeClass("table-primary")
+}
+if($('#time1').html()<time){
+  task1.addClass("table-danger")
+  task1.removeClass("table-primary")
+  }
+  if($('#time2').html()<time){
+    task2.addClass("table-danger")
+    task2.removeClass("table-primary")
+    }
+    if($('#time3').html()<time){
+      task3.addClass("table-danger")
+      task3.removeClass("table-primary")
+      }
+      if($('#time4').html()<time){
+        task4.addClass("table-danger")
+        task4.removeClass("table-primary")
+        }
+        if($('#time5').html()<time){
+          task5.addClass("table-danger")
+          task5.removeClass("table-primary")
+          }
+          if($('#time6').html()<time){
+            task6.addClass("table-danger")
+            task6.removeClass("table-primary")
+            }
+            if($('#time7').html()<time){
+              task7.addClass("table-danger")
+              task7.removeClass("table-primary")
+              }
+              if($('#time8').html()<time){
+                task8.addClass("table-danger")
+                task8.removeClass("table-primary")
+                }
+                if($('#time9').html()<time){
+                  task9.addClass("table-danger")
+                  task9.removeClass("table-primary")
+                  }
+                  if($('#time10').html()<time){
+                    task10.addClass("table-danger")
+                    task10.removeClass("table-primary")
+                    }
+                    if($('#time11').html()<time){
+                      task11.addClass("table-danger")
+                      task11.removeClass("table-primary")
+                      }
+                      if($('#time12').html()<time){
+                        task12.addClass("table-danger")
+                        task12.removeClass("table-primary")
+                        }
+                        if($('#time13').html()<time){
+                          task13.addClass("table-danger")
+                          task13.removeClass("table-primary")
+                          }
+                          if($('#time14').html()<time){
+                            task14.addClass("table-danger")
+                            task14.removeClass("table-primary")
+                            }
+                            if($('#time15').html()<time){
+                              task15.addClass("table-danger")
+                              task15.removeClass("table-primary")
+                              }
+                              if($('#time16').html()<time){
+                                task16.addClass("table-danger")
+                                task16.removeClass("table-primary")
+                                }
+                                if($('#time17').html()<time){
+                                  task17.addClass("table-danger")
+                                  task17.removeClass("table-primary")
+                                  }
+                                  if($('#time18').html()<time){
+                                    task18.addClass("table-danger")
+                                    task18.removeClass("table-primary")
+                                    }
+                                    if($('#time19').html()<time){
+                                      task19.addClass("table-danger")
+                                      task19.removeClass("table-primary")
+                                      }
+if($('#time20').html()<time){
+  task20.addClass("table-danger")
+  task20.removeClass("table-primary")
+}
+if($('#time21').html()<time){
+  task21.addClass("table-danger")
+  task21.removeClass("table-primary")
+}
+if($('#time22').html()<time){
+  task22.addClass("table-danger")
+  task22.removeClass("table-primary")
+}
+if($('#time23').html()<time){
+  task23.addClass("table-danger")
+  task23.removeClass("table-primary")
+}
+if($('#time0').html()==time){
+  task0.addClass("table-success")
+  task0.removeClass("table-primary")
+}
+if($('#time1').html()==time){
+  task1.addClass("table-success")
+  task1.removeClass("table-primary")
+}
+if($('#time2').html()==time){
+  task2.addClass("table-success")
+  task2.removeClass("table-primary")
+}
+if($('#time3').html()==time){
+  task3.addClass("table-success")
+  task3.removeClass("table-primary")
+}
+if($('#time4').html()==time){
+  task4.addClass("table-success")
+  task4.removeClass("table-primary")
+}
+if($('#time5').html()==time){
+  task5.addClass("table-success")
+  task5.removeClass("table-primary")
+}
+if($('#time6').html()==time){
+  task6.addClass("table-success")
+  task6.removeClass("table-primary")
+}
+if($('#time7').html()==time){
+  task7.addClass("table-success")
+  task7.removeClass("table-primary")
+}
+if($('#time8').html()==time){
+  task8.addClass("table-success")
+  task8.removeClass("table-primary")
+}
+if($('#time9').html()==time){
+  task9.addClass("table-success")
+  task9.removeClass("table-primary")
+}
+if($('#time10').html()==time){
+  task10.addClass("table-success")
+  task10.removeClass("table-primary")
+}
+if($('#time11').html()==time){
+  task11.addClass("table-success")
+  task11.removeClass("table-primary")
+}
+if($('#time12').html()==time){
+  task12.addClass("table-success")
+  task12.removeClass("table-primary")
+}
+if($('#time13').html()==time){
+  task13.addClass("table-success")
+  task13.removeClass("table-primary")
+}
+
+if($('#time14').html()==time){
+  task14.addClass("table-success")
+  task14.removeClass("table-primary")
+}
+if($('#time15').html()==time){
+  task15.addClass("table-success")
+  task15.removeClass("table-primary")
+}
+if($('#time16').html()==time){
+  task16.addClass("table-success")
+  task16.removeClass("table-primary")
+}
+if($('#time17').html()==time){
+  task17.addClass("table-success")
+  task17.removeClass("table-primary")
+}
+if($('#time18').html()==time){
+  task18.addClass("table-success")
+  task18.removeClass("table-primary")
+}
+if($('#time19').html()==time){
+  task19.addClass("table-success")
+  task19.removeClass("table-primary")
+}
+if($('#time20').html()==time){
+  task20.addClass("table-success")
+  task20.removeClass("table-primary")
+}
+if($('#time21').html()==time){
+  task21.addClass("table-success")
+  task21.removeClass("table-primary")
+}
+if($('#time22').html()==time){
+  task22.addClass("table-success")
+  task22.removeClass("table-primary")
+}
+if($('#time23').html()==time){
+  task23.addClass("table-success")
+  task23.removeClass("table-primary")
+}
+
+
